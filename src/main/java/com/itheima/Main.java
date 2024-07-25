c8e6111cc611984d62be15fe82b1ffa5e92b5281
lonelykkk <2765314967@qq.com>
2024-07-25 17:59:32 +0800

    测试

---
+++ src/main/java/com/itheima/Main.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+/**
+ * @author lonelykkk
+ * @email 2765314967@qq.com
+ * @date ${DATE} ${TIME}
+ * @Version V1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
